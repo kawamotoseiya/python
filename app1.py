294f90756b9545fe3df8e35c9f8f8ca8015fc88c
kawamotoseiya <seinnto1226@icloud.com>
2020-09-10 04:48:39 +0900

    pillow

--- app1.py
+++ app1.py
@@ -1,13 +1,15 @@
-
 import sys
 sys.path.append("/Users/kawamotoseiya/Library/Python/3.7/lib/python/site-packages")
 
+def dispLabel():
+    lbl.configure(text="こんにちは")
+    
 import tkinter as tk
 root = tk.Tk()
 root.geometry("200x100")
 
 lbl = tk.Label(text="LABEL")
-btn = tk.Button(text="PUSH")
+btn = tk.Button(text="PUSH", command = dispLabel)
 lbl.pack()
 btn.pack()
 tk.mainloop()
