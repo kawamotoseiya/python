02558c7bac11aa4c554f77bb46dd9d1bbafa67b3
kawamotoseiya <seinnto1226@icloud.com>
2020-09-12 16:00:38 +0900

    news

--- news.py
+++ news.py
@@ -1,10 +1,14 @@
 import sys
 sys.path.append("/Users/kawamotoseiya/Library/Python/3.7/lib/python/site-packages")
+
 import requests
 from bs4 import BeautifulSoup
 
-load_url = "https://reajoy.net/shingeki-new/31516/"
+load_url = "https://news.yahoo.co.jp/categories/it"
+
 html = requests.get(load_url)
 soup = BeautifulSoup(html.content, "html.parser")
 
-print(soup.find("title"))
+topic = soup.find(class_= "topicsList_main")
+for element in topic.find_all("a"):
+    print(element.text)
