7ba5f752a44e8d3d7239c4235239861fce462b62
kawamotoseiya <seinnto1226@icloud.com>
2020-09-13 15:41:38 +0900

    update

--- image_request.py
+++ image_request.py
@@ -7,7 +7,7 @@ from pathlib import Path
 out_folder = Path("download")
 out_folder.mkdir(exist_ok=True)
 
-image_url = "https://www.ymori.com/bookspython2nen/sample1.png"
+image_url = "https://www.ymori.com/books/python2nen/sample1.png"
 imgdata = requests.get(image_url)
 
 filename = image_url.split("/")[-1]
