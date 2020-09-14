4523f730db51dd26996ee8f2ff05f71cdd00988a
kawamotoseiya <seinnto1226@icloud.com>
2020-09-14 19:46:44 +0900

    update

--- chap3.py
+++ chap3.py
@@ -2,7 +2,11 @@ import sys
 sys.path.append("/Users/kawamotoseiya/Library/Python/3.7/lib/python/site-packages")
 
 import pandas as pd
-
 df = pd.read_csv("test.csv")
-print(df)
+
+print("date=",len(df))
+print("name=",df.columns.values)
+print("index=",df.index.values)
+
+print("mass\n",df["数学"].max())
 
