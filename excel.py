960b1fd4105bf7fce797619ea0662d9ef6c6e7fd
kawamotoseiya <seinnto1226@icloud.com>
2020-09-21 15:31:26 +0900

    excel

---
+++ excel.py
@@ -0,0 +1,6 @@
+import pandas as pd
+import openpyxl
+
+df = pd.read_csv("test.csv")
+kokugo = df.sort_values("国語",ascending=False)
+kokugo.to_excel("csv_to_excell2.xlsx", index=False, sheet_name="国語でソート")
